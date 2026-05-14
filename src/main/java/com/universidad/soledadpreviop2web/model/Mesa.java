aa5178068ebb1f69ca2e385aab32d0fb3cdedfb4
SebasSoledad07 <sebastiansg@ufps.edu.co>
2026-05-14 07:42:23 -0400

    feat: agregar clases Mesa y Reserva con atributos y métodos básicos

--- src/main/java/com/universidad/soledadpreviop2web/model/Mesa.java
+++ src/main/java/com/universidad/soledadpreviop2web/model/Mesa.java
@@ -1,5 +1,53 @@
 package com.universidad.soledadpreviop2web.model;
 
 public class Mesa {
+	private int id;
+	private int numero;
+	private int capacidad;
+	private String zona;
+	private boolean activa;
+
+	public Mesa() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public String getZona() {
+		return zona;
+	}
+
+	public void setZona(String zona) {
+		this.zona = zona;
+	}
+
+	public boolean isActiva() {
+		return activa;
+	}
+
+	public void setActiva(boolean activa) {
+		this.activa = activa;
+	}
 }
 
--- src/main/java/com/universidad/soledadpreviop2web/model/Reserva.java
+++ src/main/java/com/universidad/soledadpreviop2web/model/Reserva.java
@@ -1,5 +1,71 @@
 package com.universidad.soledadpreviop2web.model;
 
 public class Reserva {
+	private int id;
+	private int mesaId;
+	private String clienteNombre;
+	private String fecha;
+	private String horaInicio;
+	private String horaFin;
+	private int numPersonas;
+
+	public Reserva() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getMesaId() {
+		return mesaId;
+	}
+
+	public void setMesaId(int mesaId) {
+		this.mesaId = mesaId;
+	}
+
+	public String getClienteNombre() {
+		return clienteNombre;
+	}
+
+	public void setClienteNombre(String clienteNombre) {
+		this.clienteNombre = clienteNombre;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(String horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public String getHoraFin() {
+		return horaFin;
+	}
+
+	public void setHoraFin(String horaFin) {
+		this.horaFin = horaFin;
+	}
+
+	public int getNumPersonas() {
+		return numPersonas;
+	}
+
+	public void setNumPersonas(int numPersonas) {
+		this.numPersonas = numPersonas;
+	}
 }
 
